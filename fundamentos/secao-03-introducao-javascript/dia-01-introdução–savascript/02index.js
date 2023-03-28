f771f0ea4a9243e02a260e41a2f52946e8c72894
Braz Cruz <braz_bmdc@hotmail.com>
2023-03-28 17:13:29 -0300

    Js aula 01

---
+++ fundamentos/secao-03-introducao-javascript/dia-01-introdução–savascript/02index.js
@@ -0,0 +1,16 @@
+// let movie = 'Avengers';  string literal
+// let score = 10.893; number literal
+// let name; undefined
+// let color = null; redefinição 
+
+
+let base = 5;
+let height = 8;
+let area = base * height;
+
+let perimeter = 5 + 5 + 8 + 8;
+console.log(perimeter)
+
+console.log(area);
+
+
