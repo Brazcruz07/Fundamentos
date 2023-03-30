f2b5e99e8eccd2e3f1d350a590f552ec9c2df402
Braz Cruz <braz_bmdc@hotmail.com>
2023-03-30 09:44:04 -0300

    Exs de JS

---
+++ fundamentos/secao-03-introducao-javascript/dia-02-introdução–javascript/00index.js
@@ -0,0 +1,16 @@
+let semaforo = 'Vermelho';
+
+
+if (semaforo === 'Verde') {
+    mensagem = 'Pode passar!'
+    console.log(mensagem)
+} 
+else if (semaforo === 'Amarelo') {
+    mensagem = 'Antenção!!'
+    console.log(mensagem)
+}
+
+else if (semaforo === 'Vermelho') {
+    mensagem = 'Paree!!!'
+    console.log(mensagem)
+}
